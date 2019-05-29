4067339a8e5c66c360d7e718437082aa0f17f6a7
Alex Dearden <alex.dearden@farepilot.com>
2019-05-29 16:21:15 +0100

    Initial Commit

---
+++ ClearScore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearScore
+//
+//  Created by Alex Dearden on 29/05/2019.
+//  Copyright © 2019 Alex Dearden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
